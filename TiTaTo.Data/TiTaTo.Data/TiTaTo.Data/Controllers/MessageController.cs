57e4362767780a9c8ea35d078a0d25b7b05f09be
Derek Lee <derekleekokfui@gmail.com>
2017-06-08 08:07:59 -0500

    reorganized files.  Added framework for WebAPI.  added .gitignore

---
+++ TiTaTo.Data/TiTaTo.Data/TiTaTo.Data/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TiTaTo.Data.Models;
+
+namespace TiTaTo.Data.Controllers
+{
+    public class MessageController : ApiController
+    {
+        Message[] messages = new Message[]
+        {
+            new Message { ID = 1, Sender = "Derek", Receiver = "Benedict", Content = "Hello"},
+            new Message { ID = 2, Sender = "Benedict", Receiver = "Derek", Content = "Hello Back!"},
+            new Message { ID = 3, Sender = "Derek", Receiver = "Benedict", Content = "Goodbye"},
+            new Message { ID = 4, Sender = "Derek", Receiver = "Adrian", Content = "Hello"}
+        };
+
+        public IEnumerable<Message> GetAllMessages()
+        {
+            return messages;
+        }
+
+        public IHttpActionResult GetMessage(int ID)
+        {
+            var message = messages.FirstOrDefault(m => m.ID == ID);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            return Ok(message);
+        }
+    }
+}
---
+++ TiTaTo.Data/TiTaTo.Data/TiTaTo.Data/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TiTaTo.Data.Models
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+    }
+}
