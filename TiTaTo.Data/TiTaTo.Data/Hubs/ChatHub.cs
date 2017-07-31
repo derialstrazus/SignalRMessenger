8d1508266fb949d75863d723f1f6df45b21904b6
kokd <kokd@CH1WIN509>
2017-07-31 10:57:25 -0500

    Revert "Added reset page and made it default.  Got hubs working.  Added join hub.  Added empty hub functions."
    This reverts commit bb63ec023b2d5bd8c5a1d353ce92f388444bd61f.

--- TiTaTo.Data/TiTaTo.Data/Hubs/ChatHub.cs
+++ TiTaTo.Data/TiTaTo.Data/Hubs/ChatHub.cs
@@ -1,75 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
-using TiTaTo.Data.DataAccess;
-using TiTaTo.Data.Models;
 
 namespace TiTaTo.Data.Hubs
 {
-    public class ChatHub : Hub
-    {
-        SingletonDB s1 = SingletonDB.Instance;
-
-        public void Join(string playerID)
-        {
-            Guid playerGUID;
-            bool parseSuccess = Guid.TryParse(playerID, out playerGUID);
-            if (!parseSuccess)
-            {
-                throw new HubException("Improper GUID passed.");
-            }
-
-            User user = s1.Users.FirstOrDefault(u => u.ID == playerGUID);
-            if (user == null)
-            {
-                //Clients.Caller.userDoesNotExist();
-                throw new HubException("User does not exist");                
-            }
-
-            user.ConnectionID = Context.ConnectionId;
-            //Notify all users
-            Clients.All.SomeoneJoined(user.Name);
-        }
-
-        public void Send(string name, string message)
-        {
-            Clients.All.broadcastMessage(name, message);
-        }
-
-        public void CreateRoom()
-        {
-            //add room to db
-            //notify all users that are invited to this room
-        }
-
-        public void UserOnline()
-        {
-            //add user to db
-            //notify all users that someone is online and userlist needs to be updated
-        }
-
-        public void SendMessage(string message, string userID, string destinationChatRoom)
-        {
-            //add message to db
-            //notify all users who are receivers
-        }
-
-
-
-
-
-
-        public override Task OnDisconnected(bool stopCalled)
-        {
-            User user = s1.Users.FirstOrDefault(u => u.ConnectionID == Context.ConnectionId);
-            if (user != null)
-            {
-                user.ConnectionID = null;
-            }
-            return base.OnDisconnected(stopCalled);
-        }
-    }
+   public class ChatHub : Hub
+   {
+      public void Send(string name, string message)
+      {
+         Clients.All.broadcastMessage(name, message);
+      }
+   }
 }
